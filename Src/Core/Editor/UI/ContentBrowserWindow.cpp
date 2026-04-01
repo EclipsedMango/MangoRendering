6898a4be6f936999a5247c626a66fffa96679a7f
EclipsedMango <140897468+EclipsedMango@users.noreply.github.com>
2026-04-02 08:17:16 +1100

    added multi selecting

--- Src/Core/Editor/UI/ContentBrowserWindow.cpp
+++ Src/Core/Editor/UI/ContentBrowserWindow.cpp
@@ -1,12 +1,15 @@
 
 #include "ContentBrowserWindow.h"
 
+#include <algorithm>
 #include <fstream>
 
 #include "imgui.h"
 #include "Core/Editor/EditorStyle.h"
 
-ContentBrowserWindow::ContentBrowserWindow(Editor *editor) : m_editor(editor), m_currentPath(fs::current_path() / "Root") {}
+ContentBrowserWindow::ContentBrowserWindow(Editor *editor) : m_editor(editor), m_currentPath(fs::current_path() / "Root") {
+    RefreshDirectory();
+}
 
 void ContentBrowserWindow::DrawContentBrowser() {
     ImGui::Begin("Content Browser");
@@ -20,6 +23,8 @@ void ContentBrowserWindow::DrawContentBrowser() {
     if (ImGui::Button("Back", ImVec2(64, 32))) {
         if (m_currentPath.filename() != "Root") {
             m_currentPath = m_currentPath.parent_path();
+            RefreshDirectory();
+            m_selection.Clear();
         }
     }
 
@@ -29,6 +34,8 @@ void ContentBrowserWindow::DrawContentBrowser() {
         } else if (ImGui::GetTime() - m_backHoverStart >= m_backHoverDelay) {
             if (m_currentPath.filename() != "Root") {
                 m_currentPath = m_currentPath.parent_path();
+                RefreshDirectory();
+                m_selection.Clear();
             }
             m_backHoverStart = 0.0;
         }
@@ -37,138 +44,220 @@ void ContentBrowserWindow::DrawContentBrowser() {
     }
 
     ImGui::Separator();
-    DisplayPath(m_currentPath);
 
-    if (m_backHoverStart == 0.0) {
-        ImGui::SetNextItemAllowOverlap();
-        ImGui::SetCursorScreenPos(ImGui::GetWindowPos());
-        ImGui::InvisibleButton("##window_drop_target", ImGui::GetWindowSize());
-        AcceptDrop(m_currentPath);
+    ImGui::BeginChild("##content_browser_region", ImVec2(0, 0), false);
 
-        if (ImGui::IsItemClicked(ImGuiMouseButton_Right)) {
-            ImGui::OpenPopup("##bg_context");
-        }
+    DisplayPath();
+
+    if (ImGui::IsWindowHovered() && !ImGui::IsAnyItemHovered() && ImGui::IsMouseClicked(ImGuiMouseButton_Left)) {
+        m_selection.Clear();
     }
 
-    if (ImGui::BeginPopup("##bg_context")) {
-        if (ImGui::MenuItem("New Folder")) NewFolder(m_currentPath);
-        if (ImGui::MenuItem("New File")) NewFile(m_currentPath);
+    if (ImGui::BeginPopupContextWindow("##bg_context",
+        ImGuiPopupFlags_MouseButtonRight | ImGuiPopupFlags_NoOpenOverItems)) {
+        if (ImGui::MenuItem("New Folder")) {
+            NewFolder(m_currentPath);
+            RefreshDirectory();
+        }
+
+        if (ImGui::MenuItem("New File")) {
+            NewFile(m_currentPath);
+            RefreshDirectory();
+        }
         ImGui::EndPopup();
     }
+
+    ImGui::EndChild();
+
+    AcceptDrop(m_currentPath);
+    FlushPendingPathChange();
 
     ImGui::End();
 }
 
-void ContentBrowserWindow::DisplayPath(const fs::path &path) {
+void ContentBrowserWindow::DisplayPath() {
     constexpr float itemSize = 80.0f;
     constexpr float padding = 10.0f;
+
     const float windowWidth = ImGui::GetContentRegionAvail().x;
     const int columns = std::max(1, static_cast<int>(windowWidth / (itemSize + padding)));
 
-    int i = 0;
-    for (const auto& entry : fs::directory_iterator(path)) {
-        std::string filename = entry.path().filename().string();
-        std::string fullPath = entry.path().string();
+    ImGuiMultiSelectFlags ms_flags = ImGuiMultiSelectFlags_ClearOnEscape;
 
-        if (i % columns != 0) {
-            ImGui::SameLine(0, padding);
-        }
-
-        ImGui::BeginGroup();
-
-        ImVec2 startPos = ImGui::GetCursorScreenPos();
-
-        ImGui::PushID(filename.c_str());
-        const bool clicked = ImGui::InvisibleButton("##item_hitbox", ImVec2(itemSize, itemSize));
-        const bool rightClick = ImGui::IsItemClicked(ImGuiMouseButton_Right);
-        const bool hovered = ImGui::IsItemHovered();
-        ImGui::PopID();
-
-        ImDrawList* drawList = ImGui::GetWindowDrawList();
-
-        if (hovered) {
-            drawList->AddRectFilled(startPos, ImVec2(startPos.x + itemSize, startPos.y + itemSize), IM_COL32(255, 255, 255, 20), 4.0f);
-        }
-
-        const char* icon;
-        ImU32 iconColor = IM_COL32_WHITE;
-        ImVec2 iconOffset = ImVec2(0, 0);
-
-        if (entry.is_directory()) {
-            if (clicked) {
-                m_currentPath = entry.path();
-            }
-
-            if (rightClick) {
-                ImGui::OpenPopup("##fol_context");
-                m_contextPayloadPath = relative(entry.path());
-            }
-
-            icon = "\uf114";
-            iconColor = IM_COL32(180, 180, 180, 255);
-            iconOffset = ImVec2(-7.5, 0);
-
-            AcceptDrop(entry.path());
-            StartDrag(fullPath, filename);
-        } else {
-            if (clicked) {
-                // TODO: handle file selection
-            }
-
-            icon = "\uf016";
-            iconColor = IM_COL32(180, 180, 180, 255);
-            iconOffset = ImVec2(-2.5, 0);
-
-            StartDrag(fullPath, filename);
-        }
-
-        ImGui::PushFont(EditorStyle::Get().mainFontExLg);
-        const ImVec2 iconSize = ImGui::CalcTextSize(icon);
-        drawList->AddText(ImVec2(startPos.x + (itemSize - iconSize.x) * 0.5f + iconOffset.x, startPos.y + iconOffset.y), iconColor, icon);
-        ImGui::PopFont();
-
-        const ImVec2 textSize = ImGui::CalcTextSize(filename.c_str());
-        float textX = startPos.x + (itemSize - textSize.x) * 0.5f;
-
-        if (textSize.x > itemSize) textX = startPos.x;
-
-        drawList->PushClipRect(startPos, ImVec2(startPos.x + itemSize, startPos.y + itemSize), true);
-        drawList->AddText(ImGui::GetFont(), ImGui::GetFontSize(), ImVec2(textX, startPos.y + itemSize - textSize.y - 2.0f), IM_COL32(230, 230, 230, 255), filename.c_str());
-        drawList->PopClipRect();
-
-        ImGui::EndGroup();
-        i++;
+    if (ImGui::GetIO().KeyShift) {
+        ms_flags |= ImGuiMultiSelectFlags_BoxSelect2d;
     }
 
-    if (ImGui::BeginPopup("##fol_context")) {
-        if (ImGui::MenuItem("Open Folder")) {
-            m_currentPath = m_contextPayloadPath;
+    ImGuiMultiSelectIO* ms_io = ImGui::BeginMultiSelect(ms_flags, m_selection.Size, static_cast<int>(m_currentDirEntries.size()));
+    m_selection.ApplyRequests(ms_io);
+
+    for (int n = 0; n < static_cast<int>(m_currentDirEntries.size()); ++n) {
+        if (n % columns != 0) {
+            ImGui::SameLine(0.0f, padding);
+        }
+
+        ImGui::SetNextItemSelectionUserData(n);
+        DrawEntry(m_currentDirEntries[n], n);
+    }
+
+    ms_io = ImGui::EndMultiSelect();
+    m_selection.ApplyRequests(ms_io);
+}
+
+void ContentBrowserWindow::DrawEntry(const fs::directory_entry& entry, int index) {
+    constexpr float itemSize = 80.0f;
+    const std::string filename = entry.path().filename().string();
+
+    ImGui::PushID(index);
+    ImGui::BeginGroup();
+
+    const ImVec2 startPos = ImGui::GetCursorScreenPos();
+    const bool isSelected = m_selection.Contains(static_cast<ImGuiID>(index));
+
+    constexpr ImGuiSelectableFlags flags = ImGuiSelectableFlags_AllowDoubleClick | ImGuiSelectableFlags_AllowOverlap;
+
+    ImGui::Selectable("##item", isSelected, flags, ImVec2(itemSize, itemSize));
+
+    const bool hovered = ImGui::IsItemHovered();
+    const bool rightClicked = ImGui::IsItemClicked(ImGuiMouseButton_Right);
+
+    StartDrag(entry.path().string(), filename);
+    if (entry.is_directory()) {
+        AcceptDrop(entry.path());
+    }
+
+    if (rightClicked && !isSelected) {
+        m_selection.Clear();
+        m_selection.SetItemSelected(static_cast<ImGuiID>(index), true);
+    }
+
+    if (hovered && ImGui::IsMouseDoubleClicked(ImGuiMouseButton_Left)) {
+        if (entry.is_directory()) {
+            QueuePathChange(entry.path());
+        }
+    }
+
+    const bool drawSelected = isSelected || (rightClicked && !isSelected);
+
+    ImDrawList* drawList = ImGui::GetWindowDrawList();
+    if (drawSelected) {
+        drawList->AddRectFilled(startPos, ImVec2(startPos.x + itemSize, startPos.y + itemSize), IM_COL32(40, 90, 150, 200), 4.0f);
+    } else if (hovered) {
+        drawList->AddRectFilled(startPos, ImVec2(startPos.x + itemSize, startPos.y + itemSize), IM_COL32(255, 255, 255, 30), 4.0f);
+    }
+
+    if (entry.is_directory()) {
+        DrawEntryFolder(startPos, itemSize);
+    } else {
+        DrawEntryFile(startPos, itemSize);
+    }
+
+    const ImVec2 textSize = ImGui::CalcTextSize(filename.c_str());
+    float textX = startPos.x + (itemSize - textSize.x) * 0.5f;
+    if (textSize.x > itemSize) {
+        textX = startPos.x;
+    }
+
+    drawList->PushClipRect(startPos, ImVec2(startPos.x + itemSize, startPos.y + itemSize), true);
+    drawList->AddText(ImGui::GetFont(), ImGui::GetFontSize(), ImVec2(textX, startPos.y + itemSize - textSize.y - 2.0f), IM_COL32(230, 230, 230, 255), filename.c_str());
+    drawList->PopClipRect();
+
+    if (ImGui::BeginPopupContextItem("##entry_context")) {
+        if (entry.is_directory() && ImGui::MenuItem("Open Folder")) {
+            QueuePathChange(entry.path());
         }
 
         ImGui::Separator();
 
         if (ImGui::MenuItem("Copy Path")) {
-            const std::string pathStr = m_contextPayloadPath.string();
+            const std::string pathStr = entry.path().string();
             ImGui::SetClipboardText(pathStr.c_str());
         }
+
         if (ImGui::MenuItem("Copy Absolute Path")) {
-            const std::string absPathStr = fs::absolute(m_contextPayloadPath).string();
+            const std::string absPathStr = fs::absolute(entry.path()).string();
             ImGui::SetClipboardText(absPathStr.c_str());
         }
 
         ImGui::Separator();
 
-        if (ImGui::MenuItem("Duplicate Folder")) {}
-        if (ImGui::MenuItem("Rename Folder")) {}
-        if (ImGui::MenuItem("Delete Folder")) {}
+        if (entry.is_directory() && ImGui::MenuItem("Duplicate Folder")) {
+            // TODO
+        }
+        if (ImGui::MenuItem("Rename")) {
+            // TODO
+        }
+        if (ImGui::MenuItem("Delete")) {
+            // TODO
+        }
 
         ImGui::Separator();
 
-        if (ImGui::MenuItem("Open in Terminal")) {}
-        if (ImGui::MenuItem("Open in File Manager")) {}
+        if (ImGui::MenuItem("Open in Terminal")) {
+            // TODO
+        }
+        if (ImGui::MenuItem("Open in File Manager")) {
+            // TODO
+        }
 
         ImGui::EndPopup();
     }
+
+    ImGui::EndGroup();
+    ImGui::PopID();
+}
+
+void ContentBrowserWindow::DrawEntryFile(const ImVec2 &startPos, const float itemSize) {
+    const char* icon = "\uf016";
+    constexpr ImU32 iconColor = IM_COL32(180, 180, 180, 255);
+    constexpr ImVec2 iconOffset = ImVec2(-2.5f, 0.0f);
+
+    ImDrawList* drawList = ImGui::GetWindowDrawList();
+
+    ImGui::PushFont(EditorStyle::Get().mainFontExLg);
+    const ImVec2 iconSize = ImGui::CalcTextSize(icon);
+    drawList->AddText(ImVec2(startPos.x + (itemSize - iconSize.x) * 0.5f + iconOffset.x, startPos.y + iconOffset.y), iconColor, icon);
+    ImGui::PopFont();
+}
+
+void ContentBrowserWindow::DrawEntryFolder(const ImVec2 &startPos, const float itemSize) {
+    const char* icon = "\uf114";
+    constexpr ImU32 iconColor = IM_COL32(180, 180, 180, 255);
+    constexpr ImVec2 iconOffset = ImVec2(-7.5f, 0.0f);
+
+    ImDrawList* drawList = ImGui::GetWindowDrawList();
+
+    ImGui::PushFont(EditorStyle::Get().mainFontExLg);
+    const ImVec2 iconSize = ImGui::CalcTextSize(icon);
+    drawList->AddText(ImVec2(startPos.x + (itemSize - iconSize.x) * 0.5f + iconOffset.x, startPos.y + iconOffset.y), iconColor, icon);
+    ImGui::PopFont();
+}
+
+void ContentBrowserWindow::RefreshDirectory() {
+    m_currentDirEntries.clear();
+    for (const auto& entry : fs::directory_iterator(m_currentPath)) {
+        m_currentDirEntries.push_back(entry);
+    }
+
+    // TODO: sort m_currentDirEntries so folders are first or something
+}
+
+void ContentBrowserWindow::QueuePathChange(const fs::path &newPath) {
+    m_pendingPathChange = newPath;
+    m_hasPendingPathChange = true;
+}
+
+void ContentBrowserWindow::FlushPendingPathChange() {
+    if (!m_hasPendingPathChange) {
+        return;
+    }
+
+    m_currentPath = m_pendingPathChange;
+    RefreshDirectory();
+    m_selection.Clear();
+
+    m_hasPendingPathChange = false;
+    m_pendingPathChange.clear();
 }
 
 void ContentBrowserWindow::NewFolder(const fs::path &path) {
@@ -216,21 +305,40 @@ void ContentBrowserWindow::NewFile(fs::path &path) {
 }
 
 void ContentBrowserWindow::StartDrag(const std::string &fullPath, const std::string &filename) {
-    if (ImGui::BeginDragDropSource(ImGuiDragDropFlags_SourceAllowNullID)) {
+    if (ImGui::BeginDragDropSource(ImGuiDragDropFlags_SourceNoDisableHover)) {
         ImGui::SetDragDropPayload("FILE_PATH", fullPath.c_str(), fullPath.size() + 1);
         ImGui::Text("Dragging: %s", filename.c_str());
         ImGui::EndDragDropSource();
     }
 }
 
-void ContentBrowserWindow::AcceptDrop(const fs::path &destDir) {
+bool ContentBrowserWindow::AcceptDrop(const fs::path& destDir) {
+    bool moved = false;
+    fs::path sourcePath;
+
     if (ImGui::BeginDragDropTarget()) {
         if (const ImGuiPayload* payload = ImGui::AcceptDragDropPayload("FILE_PATH")) {
-            const fs::path sourcePath = static_cast<const char*>(payload->Data);
+            sourcePath = static_cast<const char*>(payload->Data);
             try {
-                fs::rename(sourcePath, destDir / sourcePath.filename());
-            } catch (const fs::filesystem_error& e) {}
+                const fs::path targetPath = destDir / sourcePath.filename();
+                if (sourcePath != targetPath) {
+                    fs::rename(sourcePath, targetPath);
+                    moved = true;
+                }
+            } catch (const fs::filesystem_error&) {}
         }
         ImGui::EndDragDropTarget();
     }
+
+    if (moved) {
+        const bool sourceWasVisible = (sourcePath.parent_path() == m_currentPath);
+        const bool destIsVisible = (destDir == m_currentPath);
+
+        if (sourceWasVisible || destIsVisible) {
+            RefreshDirectory();
+            m_selection.Clear();
+        }
+    }
+
+    return moved;
 }
--- Src/Core/Editor/UI/ContentBrowserWindow.h
+++ Src/Core/Editor/UI/ContentBrowserWindow.h
@@ -3,6 +3,9 @@
 #define MANGORENDERING_CONTENTBROWSERWINDOW_H
 
 #include <filesystem>
+#include <vector>
+
+#include "imgui.h"
 
 class Editor;
 
@@ -16,19 +19,33 @@ public:
     void DrawContentBrowser();
 
 private:
-    void DisplayPath(const fs::path& path);
+    void DisplayPath();
+    void DrawEntry(const fs::directory_entry& entry, int index);
+    static void DrawEntryFile(const ImVec2& startPos, float itemSize);
+    static void DrawEntryFolder(const ImVec2& startPos, float itemSize);
+    void RefreshDirectory();
+
+    void QueuePathChange(const fs::path& newPath);
+    void FlushPendingPathChange();
+
     static void NewFolder(const fs::path& path);
     static void NewFile(fs::path& path);
 
     static void StartDrag(const std::string& fullPath, const std::string& filename);
-    static void AcceptDrop(const fs::path& destDir);
+    bool AcceptDrop(const fs::path& destDir);
 
     Editor* m_editor = nullptr;
     fs::path m_currentPath;
     fs::path m_contextPayloadPath;
 
+    std::vector<fs::directory_entry> m_currentDirEntries;
+    ImGuiSelectionBasicStorage m_selection;
+
     double m_backHoverStart = 0.0;
     const double m_backHoverDelay = 0.4;
+
+    fs::path m_pendingPathChange;
+    bool m_hasPendingPathChange = false;
 };
 
 
